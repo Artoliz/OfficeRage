e85b0434b84c2f00f322d6a31e2690896183d4eb
Charles Descoust <charles.descoust@epitech.eu>
2019-11-25 11:38:17 +0900

    Room 1

---
+++ Office Rage/Assets/Scripts/AddForceOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceOnClick : MonoBehaviour
+{
+    public float Force;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, Force));
+        }
+    }
+}
