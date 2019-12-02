cc7098c3471897a0d9acb92460a50c2b35e45465
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-12-02 12:51:03 +0100

    Begin Introduction Scene

---
+++ Office Rage/Assets/Scripts/Introduction/MoveHead.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MoveHead : MonoBehaviour
+{
+    private float _xRotation;
+    private float _yRotation;
+    private Transform _transform;
+    
+    public float mouseSensitivity = 100f;
+
+/*    private void Awake()
+    {
+        _transform = gameObject.transform;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 50f);
+
+        _yRotation += mouseX;
+        _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+
+        _transform.localRotation = Quaternion.Euler(_xRotation, _yRotation, 0f);
+    }*/
+}
