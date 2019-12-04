48fb9424ba38129a06d742ac2df38f7fd4e758cb
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-12-04 15:31:53 +0100

    Pause menu Done

---
+++ Office Rage/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused;
+    public GameObject pauseMenuUi;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Introduction");
+    }
+    
+    public void ExitToDesktop()
+    {
+        Application.Quit();
+    }
+}
