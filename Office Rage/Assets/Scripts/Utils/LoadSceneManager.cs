b8854ab0a1be8036571acdb53109d0e637ed34ec
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-12-05 10:23:08 +0100

    Create Load scene manager fading between scenes

---
+++ Office Rage/Assets/Scripts/Utils/LoadSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    private string _scene;
+    private static readonly int FadeOut = Animator.StringToHash("FadeOut");
+
+    public Animator anim;
+
+    public static LoadSceneManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LoadLevel(string scene)
+    {
+        _scene = scene;
+        anim.SetTrigger(FadeOut);
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(_scene);
+    }
+}
