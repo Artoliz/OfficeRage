57e937ca252ce011f40544ff186b04d9311a60b0
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-12-06 08:31:59 +0100

    First big push for the end scene, it's working like excpected. The test is displaying, click to pass, escape to quit

---
+++ Office Rage/Assets/Scripts/EndScene/TextWriter.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    #region PrivateVariables
+
+    private static TextWriter _instance;
+
+    private List<TextWriterSingle> _textWriterSingleList;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        _instance = this;
+        _textWriterSingleList = new List<TextWriterSingle>();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _textWriterSingleList.Count; i++)
+        {
+            bool destroyInstance = _textWriterSingleList[i].Update();
+            if (destroyInstance)
+            {
+                _textWriterSingleList.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private TextWriterSingle AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+    {
+        TextWriterSingle textWriterSingle =
+            new TextWriterSingle(uiText, textToWrite, timePerCharacter, invisibleCharacters);
+        _textWriterSingleList.Add(textWriterSingle);
+        
+        return textWriterSingle;
+    }
+
+    public static void RemoveWriterStatic(Text uiText)
+    {
+        _instance.RemoveWriter(uiText);
+    }
+
+    private void RemoveWriter(Text uiText)
+    {
+        for (int i = 0; i < _textWriterSingleList.Count; i++)
+        {
+            if (_textWriterSingleList[i].GetUiText() == uiText)
+            {
+                _textWriterSingleList.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    #endregion
+
+    #region PublicMethods
+    
+    public static TextWriterSingle AddWriterStatic(Text uiText, string textToWrite, float timePerCharacter,
+        bool invisibleCharacters, bool removeWriterBeforeAdd)
+    {
+        if (removeWriterBeforeAdd)
+            _instance.RemoveWriter(uiText);
+        return _instance.AddWriter(uiText, textToWrite, timePerCharacter, invisibleCharacters);
+    }
+
+    #endregion
+
+    // Represents a single TextWriter instance
+    public class TextWriterSingle
+    {
+        private readonly Text _uiText;
+        private readonly string _textToWrite;
+        private int _characterIndex;
+        private readonly float _timePerCharacter;
+        private float _timer;
+        private readonly bool _invisibleCharacters;
+
+        // Returns true on complete
+        public bool Update()
+        {
+            _timer -= Time.deltaTime;
+            while (_timer <= 0f)
+            {
+                // Display next character
+                _timer += _timePerCharacter;
+                _characterIndex++;
+                string text = _textToWrite.Substring(0, _characterIndex);
+                if (_invisibleCharacters)
+                {
+                    text += "<color=#00000000>" + _textToWrite.Substring(_characterIndex) + "</color>";
+                }
+
+                _uiText.text = text;
+
+                if (_characterIndex >= _textToWrite.Length)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public TextWriterSingle(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+        {
+            _uiText = uiText;
+            _textToWrite = textToWrite;
+            _timePerCharacter = timePerCharacter;
+            _invisibleCharacters = invisibleCharacters;
+            _characterIndex = 0;
+        }
+        
+        public Text GetUiText()
+        {
+            return _uiText;
+        }
+
+        public bool IsActive()
+        {
+            return _characterIndex < _textToWrite.Length;
+        }
+
+        public void WriteAllAndDestroy()
+        {
+            _uiText.text = _textToWrite;
+            _characterIndex = _textToWrite.Length;
+            RemoveWriterStatic(_uiText);
+        }
+    }
+}
---
+++ Office Rage/Assets/Scripts/EndScene/UiAssistant.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAssistant : MonoBehaviour
+{
+
+    #region PrivateVariables
+
+    private int _messageIndex;
+
+    private const int WaitBeforeLaunchScene = 5;
+
+    private TextWriter.TextWriterSingle _textWriterSingle;
+    
+    [SerializeField] private Text messageText;
+
+    [SerializeField] private Button messageButton;
+
+    private readonly string[] _messages =
+    {
+        "This can be anyone:\nYour best friend.\nYour brother.\nYour sister.\nYour parents.",
+        "... even you.",
+        "Bullying is everywhere:\nIn the street.\nAt work.\nAt school.",
+        "... even at home.",
+        "You don't know what people have been trough.\nYou don't know their past.\nYou don't know what they think.",
+        "Give love.\nPeople will gave you back."
+    };
+
+    #endregion
+
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        messageButton.onClick.AddListener(ClickScreen);
+    }
+
+    private void Update()
+    {
+        if (_messageIndex == _messages.Length)
+            StartCoroutine(LaunchNextSceneAfterPause());
+        else if (Input.GetKeyDown(KeyCode.Escape))
+            LoadSceneManager.Instance.LoadLevel("Introduction");
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private void ClickScreen()
+    {
+        if (_messageIndex == _messages.Length)
+            LoadSceneManager.Instance.LoadLevel("Introduction");
+        else if (_textWriterSingle != null && _textWriterSingle.IsActive())
+            _textWriterSingle.WriteAllAndDestroy();
+        else
+        {
+            _textWriterSingle = TextWriter.AddWriterStatic(messageText, _messages[_messageIndex], .1f, true, true);
+            _messageIndex++;
+        }
+    }
+    
+    private static IEnumerator LaunchNextSceneAfterPause()
+    {
+        yield return new WaitForSeconds(WaitBeforeLaunchScene);
+        LoadSceneManager.Instance.LoadLevel("Introduction");
+    }
+
+    #endregion
+}
