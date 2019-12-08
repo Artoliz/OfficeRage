4f970d381136569bc4e2711593d9b57455a43b5b
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-12-08 06:49:11 +0100

    Both phones done

---
+++ Office Rage/Assets/Scripts/Introduction/Phones/CellPhone.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CellPhone : MonoBehaviour
+{
+    #region PrivateVariables
+
+    private Camera _camera;
+    private bool _isCameraNotNull;
+
+    private BoxCollider _boxCollider;
+
+    private bool _isVibrating;
+
+    #endregion
+
+    #region PublicVariables
+
+    public AudioSource vibrationSound;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _isCameraNotNull = _camera != null;
+
+        _boxCollider = gameObject.GetComponent<BoxCollider>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PlayVibratingSound());
+    }
+
+    private void Update()
+    {
+        if (_isCameraNotNull)
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out var hit))
+            {
+                if (hit.collider == _boxCollider)
+                {
+                    if (_isVibrating)
+                    {
+                        _isVibrating = false;
+                        vibrationSound.Stop();
+                    }
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private IEnumerator PlayVibratingSound()
+    {
+        yield return new WaitForSeconds(30f);
+        _isVibrating = true;
+        vibrationSound.Play();
+    }
+
+    #endregion
+}
---
+++ Office Rage/Assets/Scripts/Introduction/Phones/FixPhone.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPhone : MonoBehaviour
+{
+    #region PrivateVariables
+
+    private Camera _camera;
+    private bool _isCameraNotNull;
+
+    private MeshCollider _meshCollider;
+
+    private Animator _coffeeAnimator;
+
+    private bool _isDialing;
+
+    #endregion
+
+    #region PublicVariables
+
+    public AudioSource phoneDialingSound;
+    public AudioSource phoneBusySignal;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _isCameraNotNull = _camera != null;
+
+        _meshCollider = gameObject.GetComponent<MeshCollider>();
+    }
+
+    private void Update()
+    {
+        if (_isCameraNotNull && !_isDialing)
+        {
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out var hit))
+            {
+                if (hit.collider == _meshCollider)
+                {
+                    _isDialing = true;
+                    phoneDialingSound.Play();
+                    StartCoroutine(PlayDrinkSound());
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private IEnumerator PlayDrinkSound()
+    {
+        yield return new WaitForSeconds(phoneDialingSound.clip.length);
+        phoneBusySignal.Play();
+        _isDialing = false;
+    }
+
+    #endregion
+}
